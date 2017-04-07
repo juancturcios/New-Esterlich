3a31c5b53b2d6054a8a9c5315a22a06c83e1dd6e
juancturcios <juancturcios@hotmail.com>
2017-04-06 22:59:59 -0600

    Documentos principales

---
+++ contact.php
@@ -0,0 +1,117 @@
+<?php
+  include 'header.html';
+ ?>
+        <!-- Slider Start -->
+        <section id="global-header">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="block">
+                  <h1 class="text-center">CONTÁCTO</h1>
+                  <p class="text-center">Gracias por su confianza</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <!-- contact form start -->
+        <section id="contact-form">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-6 col-sm-12">
+                <div class="block">
+                  <form>
+                    <div class="form-group">
+                      <input type="text" class="form-control" placeholder="Escriba su nombre">
+                    </div>
+                    <div class="form-group">
+                      <input type="text" class="form-control" placeholder="Correo Electrónico">
+                    </div>
+                    <div class="form-group">
+                      <input type="text" class="form-control" placeholder="Asunto">
+                    </div>
+                  </form>
+                </div>
+              </div>
+              <div class="col-md-6 col-sm-12">
+                <div class="block">
+                  <form>
+                    <div class="form-group-2">
+                      <textarea class="form-control" rows="3" placeholder="Su Mensaje"></textarea>
+                    </div>
+                    <button class="btn" type="submit" style="background:black;">Enviar Mensaje</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+            <div id="contact-box" class="row">
+              <div class="col-sm-12 text-center">
+                <div class="block">
+                  <h2>Stop By For A visit</h2>
+                  <ul class="address-block">
+                    <li>
+                      <i class="fa fa-map-marker"></i>North Main Street,Brooklyn Australia
+                    </li>
+                    <li>
+                      <i class="fa fa-envelope-o"></i><a href="mailto:esterlich@esterlich-co.com">esterlich@esterlich-co.com ( mail general ) </a>
+                    </li>
+                    <li>
+                      <i class="fa fa-envelope-o"></i><a href="mailto:esterlich-hmc@esterlich-co.com">esterlich-hmc@esterlich-co.com ( ingenieria)  </a>
+                    </li>
+                    <li>
+                      <i class="fa fa-envelope-o"></i><a href="mailto:sales@polytek-ipg.com">sales@polytek-ipg.com ( catalogos y precios de tuberias)</a>
+                    </li>
+                    <li>
+                    </li>
+                    <!-- <li>
+                      <i class="fa fa-phone"></i>Phone:+88 01672 506 744
+                    </li> -->
+                  </ul>
+
+                  <!-- <ul class="social-icons">
+                    <li>
+                      <a href="#"><i class="fa fa-google"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-linkedin"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-pinterest"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-dribbble"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-behance"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-twitter"></i></a>
+                    </li>
+                    <li>
+                      <a href="#"><i class="fa fa-facebook"></i></a>
+                    </li>
+                  </ul> -->
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </section>
+        <!-- Call to action Start -->
+        <!-- <section id="call-to-action">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="block">
+                  <h2>We design delightful digital experiences.</h2>
+                  <p>Read more about what we do and our philosophy of design. Judge for yourself The work and results we’ve achieved for other clients, and meet our highly experienced Team who just love to design.</p>
+                  <a class="btn btn-default btn-call-to-action" href="#" >Tell Us Your Story</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section> -->
+
+<?php
+  include 'footer.html';
+ ?>
---
+++ index.php
@@ -0,0 +1,231 @@
+<?php
+  include 'header.html';
+ ?>
+
+    <!-- Slider Start -->
+    <section id="slider">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="block">
+              <h1 class="text-center animated fadeInUp">HOLDING GROUP</h1>
+              <p class="animated fadeInUp text-center">"NUESTRA VOCACIÓN ES DAR SOLUCIONES VIABLES, ECOLÓGICAS &#38; RÁPIDAS, ESTANDO SIEMPRE CON NUESTROS CLIENTES"</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- Wrapper Start -->
+    <section id="intro">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-7 col-sm-12">
+            <div class="block">
+              <div class="section-title">
+                <h2><b>ESTERLICH</b> & CO</h2>
+                <p>GRUPO de empresas, especializadas en la exportación e importación de productos, avalada por años de experiencia y proyectos ejecutados por todo el mundo. Desde la ingeniería, suministro, logística y financiación, aportando soluciones de llave en mano a nuestros clientes.
+                  </p>
+              </div>
+              <p>Contamos con profesionales de reconocido prestigio y experiencia en los principales mercados emergentes ofreciendo a sus clientes el asesoramiento global que necesitan en su fase de internacionalización.</p>
+            </div>
+          </div><!-- .col-md-7 close -->
+          <div class="col-md-5 col-sm-12">
+            <div class="block">
+              <img src="img/wrapper-img.jpg" alt="aqui va gif">
+            </div>
+          </div><!-- .col-md-5 close -->
+        </div>
+      </div>
+    </section>
+
+    <section id="feature">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-5 col-md-offset-7">
+
+          <h1>NOS CONFORMAMOS POR:</h1>
+          <ul>
+            <li><h4>ESTERLICH RECYCLE</h4></li>
+            <li><h4>POLYTEX SYSTEMS</h4></li>
+            <li><h4>ESTERLICH & HMC</h4></li>
+          </ul>
+
+          <a href="sobre_nosotros.php" class="btn btn-sobre-nosotros">Sobre Nosotros</a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+    <!-- Service Start -->
+    <!-- <section id="service">
+      <div class="container">
+        <div class="row">
+          <div class="section-title">
+            <h2>Our Services</h2>
+            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics</p>
+          </div>
+        </div>
+        <div class="row ">
+          <div class="col-sm-6 col-md-3">
+            <div class="service-item">
+              <i class="icon ion-coffee"></i>
+              <h4>Branding</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut </p>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-3">
+            <div class="service-item">
+              <i class="ion-compass"></i>
+              <h4>Web Design</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut </p>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-3">
+            <div class="service-item">
+              <i class="ion-image"></i>
+              <h4>App Design</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut </p>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-3">
+            <div class="service-item">
+              <i class="ion-bug"></i>
+              <h4>Start Up</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut </p>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-3">
+            <div class="service-item">
+              <i class="ion-headphone"></i>
+              <h4>Logo Design</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut </p>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-3">
+            <div class="service-item">
+              <i class="ion-leaf"></i>
+              <h4>Development</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut </p>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-3">
+            <div class="service-item">
+              <i class="ion-planet"></i>
+              <h4>Brand Identity</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut </p>
+            </div>
+          </div>
+          <div class="col-sm-6 col-md-3">
+            <div class="service-item">
+              <i class="ion-earth"></i>
+              <h4>Brand Identity</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section> -->
+    <!-- Call to action Start -->
+    <!-- <section id="call-to-action">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="block">
+              <h2>We design delightful digital experiences.</h2>
+              <p>Read more about what we do and our philosophy of design. Judge for yourself The work and results we’ve achieved for other clients, and meet our highly experienced Team who just love to design.</p>
+              <a class="btn btn-default btn-call-to-action" href="#" >Tell Us Your Story</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section> -->
+    <!-- Content Start -->
+    <!-- <section id="testimonial">
+      <div class="container">
+        <div class="row">
+          <div class="section-title text-center">
+            <h2>Fun Facts About Us</h2>
+            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="block">
+              <ul class="counter-box clearfix">
+                <li>
+                  <div class="block">
+                    <i class="ion-ios-chatboxes-outline"></i>
+                    <h4 class="counter">20000</h4>
+                    <span>Cups Of Coffee</span>
+                  </div>
+                </li>
+                <li>
+                  <div class="block">
+                    <i class="ion-ios-glasses-outline"></i>
+                    <h4 class="counter">20000</h4>
+                    <span>Cups Of Coffee</span>
+                  </div>
+                </li>
+                <li>
+                  <div class="block">
+                    <i class="ion-ios-compose-outline"></i>
+                    <h4 class="counter">20000</h4>
+                    <span>Cups Of Coffee</span>
+                  </div>
+                </li>
+                <li>
+                  <div class="block">
+                    <i class="ion-ios-timer-outline"></i>
+                    <h4 class="counter">20000</h4>
+                    <span>Cups Of Coffee</span>
+                  </div>
+                </li>
+
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="testimonial-carousel">
+              <div id="testimonial-slider" class="owl-carousel">
+                <div>
+                    <img src="img/cotation.png" alt="IMG">
+                    <p>"This Company created an e-commerce site with the tools to make our business a success, with innovative ideas we feel that our site has unique elements that make us stand out from the crowd."</p>
+                    <div class="user">
+                      <img src="img/item-img1.jpg" alt="Pepole">
+                      <p><span>Rose Ray</span> CEO</p>
+                    </div>
+                </div>
+                <div>
+                  <img src="img/cotation.png" alt="IMG">
+                    <p>"This Company created an e-commerce site with the tools to make our business a success, with innovative ideas we feel that our site has unique elements that make us stand out from the crowd."</p>
+                    <div class="user">
+                      <img src="img/item-img1.jpg" alt="Pepole">
+                      <p><span>Rose Ray</span> CEO</p>
+                    </div>
+                </div>
+                <div>
+                  <img src="img/cotation.png" alt="IMG">
+                    <p>"This Company created an e-commerce site with the tools to make our business a success, with innovative ideas we feel that our site has unique elements that make us stand out from the crowd."</p>
+                    <div class="user">
+                      <img src="img/item-img1.jpg" alt="Pepole">
+                      <p><span>Rose Ray</span> CEO</p>
+                    </div>
+                </div>
+                <div>
+                  <img src="img/cotation.png" alt="IMG">
+                    <p>"This Company created an e-commerce site with the tools to make our business a success, with innovative ideas we feel that our site has unique elements that make us stand out from the crowd."</p>
+                    <div class="user">
+                      <img src="img/item-img1.jpg" alt="Pepole">
+                      <p><span>Rose Ray</span> CEO</p>
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section> -->
+
+<?php
+  include 'footer.html';
+ ?>
