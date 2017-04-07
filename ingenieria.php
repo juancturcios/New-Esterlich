a76ca07964b876655d30823650dc5323c7982ff1
juancturcios <juancturcios@hotmail.com>
2017-04-06 23:31:53 -0600

    cosas pequeñas

---
+++ ingenieria.php
@@ -0,0 +1,106 @@
+<?php
+  include 'header.html';
+ ?>
+
+        <!-- Slider Start -->
+    <section id="global-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="block">
+                        <h1>INGENIERÍA</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+        <!-- Portfolio Start -->
+        <section id="ingenieria">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-7 col-sm-12">
+                <div class="block">
+                  <div class="title-mision">
+                    <br>
+                    <h2><b>Esterlich &</b> HMC Ingenium SA</h2>
+                  </div>
+                  <div class="text-justify">
+
+                    <br>
+                  <p>Esterlich & HMC Ingenium SA son una empresa especializada en prestar servicios de igeniería para proyectos diversos en los sectores de:
+                    <br>
+                    <br>
+                      <b>Ingeniería Hidráulica:</b> Proyectos de Riego, centrales hidroeléctricas, presas, canales de drenaje,de irrigación, potabilización y canalización.
+                    <br>
+                      <b>Ingeniería Mecánica:</b> Equipamiento electromecánico para centrales hidroeléctricas, plantas de tratamiento de residuales, estaciones de bombeo, diseños de estructuras metálicas para casa de máquina, esclusas, rompeolas, válvulas y compuertas de cierre.
+                    <br>
+                      <b>Ingeniería Civil:</b> Pasos de nivel, Presas y Derivadoras, Canales, Sistemas de Riego y Drenaje, acueductos y Alcantarillado, Estaciones de Bombeo, tratamiento de residuales.</p>
+
+                  </div>
+                </div>
+              </div><!-- .col-md-7 close -->
+              <div class="col-md-5 col-sm-12">
+                <div class="block">
+
+                </div>
+              </div><!-- .col-md-5 close -->
+            </div>
+          </div>
+        </section>
+
+        <section class="mision">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-6 col-sm-12">
+                <div class="block">
+                  <div >
+                    <h2 class="title-mision">Misión</h2>
+
+                  </div>
+                  <p class="text-justify">Somos una empresa dedicada a prestar servicios consultoría he ingeniería a nuestros clientes y aliados con el objetivo de complementar los diversos proyectos he inversiones, usando para ello modernos SOFTWARE y la experiencia de nuestro personal técnico.</p>
+                </div>
+              </div><!-- .col-md-7 close -->
+              <div class="col-md-6 col-sm-12">
+                <div class="block">
+                  <div >
+                    <h2 class="title-mision">Visión</h2>
+
+                  </div>
+                  <p class="text-justify">Ser una Empresa líder en Centroamérica, reconocida por un servicio de ingeniería de calidad y excelente relación calidad, precio, complementando con nuestra ingeniería la competitividad de nuestros aliados.</p>
+                </div>
+              </div><!-- .col-md-5 close -->
+            </div>
+          </div>
+        </section>
+
+    <!-- Clients Logo Section Start -->
+
+
+
+<!-- Call to action Start -->
+
+    <!-- footer Start
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                  <div class="footer-manu">
+                    <ul>
+                      <li><a href="#">About Us</a></li>
+                      <li><a href="#">Contact us</a></li>
+                      <li><a href="#">How it works</a></li>
+                      <li><a href="#">Support</a></li>
+                      <li><a href="#">Terms</a></li>
+                    </ul>
+                  </div>
+                  <p>Copyright &copy; Crafted by <a href="https://dcrazed.com/">Dcrazed</a>.</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+ -->
+
+
+ <?php
+   include 'footer.html';
+  ?>
