9eb572233fe984af41512bd6504de2474c367684
juancturcios <juancturcios@hotmail.com>
2017-04-11 02:43:30 -0600

    Ingenieria idea principal

--- ingenieria.php
+++ ingenieria.php
@@ -3,12 +3,13 @@
  ?>
 
         <!-- Slider Start -->
-    <section id="global-header">
+    <section id="global-header" class="header-ingenieria">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
                     <div class="block">
                         <h1>INGENIERÍA</h1>
+                        <p>DISEÑAMOS EL FUTURO </p>
                     </div>
                 </div>
             </div>
@@ -69,6 +70,110 @@
                   <p class="text-justify">Ser una Empresa líder en Centroamérica, reconocida por un servicio de ingeniería de calidad y excelente relación calidad, precio, complementando con nuestra ingeniería la competitividad de nuestros aliados.</p>
                 </div>
               </div><!-- .col-md-5 close -->
+            </div>
+          </div>
+        </section>
+
+        <section class="tercera-seccion">
+          <div class="container">
+            <h2 class="title-mision">Experencia</h2>
+            <div class="row">
+              <div class="col-md-4 col-sm-12">
+                <div class="block">
+                  <div class="thumbnail sin-fondo">
+                    <img src="img/pet.png" alt="...">
+                    <div class="caption">
+                      <hr>
+                      <h3>Centrales hidroeléctricas</h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4 col-sm-12">
+                <div class="block">
+                  <div >
+                    <div class="thumbnail sin-fondo">
+                      <img src="img/pet.png" alt="...">
+                      <div class="caption">
+                        <hr>
+                        <h3>Sistema de riego y drenaje</h3>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+              </div>
+              <div class="col-md-4 col-sm-12">
+                <div class="block">
+                  <div class="thumbnail sin-fondo">
+                    <img src="img/pet.png" alt="...">
+                    <div class="caption">
+                      <hr>
+                      <h3>Plantas de tratamiento</h3>
+                    </div>
+                  </div>
+                </div>
+              </div><!-- .col-md-4 close -->
+            </div>
+          </div>
+        </section>
+        <section id="portfolio-work" class="cuarta-seccion">
+          <div class="container">
+            <h2 class="title-mision">STAFF</h2>
+            <p>Equipo altamente calificado.</p>
+            <div class="row">
+              <div class="col-md-12">
+                <div class="block">
+
+                  <div class="portfolio-contant">
+                    <ul id="portfolio-contant-active">
+                        <li class="mix">
+                          <a href="#">
+                            <img src="img/Ing. Edgar.jpg" alt="">
+                            <div class="overly">
+                              <div class="position-center">
+                                <h2>MSC. Ing. Edgar O. Zumaquero Martínez</h2>
+                                <p>Director Ejecutivo de HMC, Ingeniero Mecánico con Maestría en Comercio Internacional, Diplomados en Gestión Económica Financiera y Finanzas Internacionales, con más de 20 años de experiencia profesional en ventas y negociaciones internacionales y exportaciones de equipos Industriales como Bombas, centrifugas, compresores, turbinas hidráulicas y proyectos de factibilidad y diseños finales.</p>
+                              </div>
+                            </div>
+                          </a>
+                      </li>
+                      <li class="mix">
+                        <a href="#">
+                          <img src="img/Ing. Ariel.jpg" alt="">
+                          <div class="overly">
+                            <div class="position-center">
+                              <h2>MSC. Ing. Ariel Enrique Ramos Hernández</h2>
+                              <p>Ingeniero Hidráulico con Maestría en Matemática e Hidráulica, Diplomado en Marketing y Negociación y Auditor de Sistemas de Gestión de Calidad de la LLOYDs. Especialista Superior de Proyectos e Investigaciones Hidrológicas con más de 20 años de experiencia profesional. Ha incursionado como Ingeniero a Pie de Obra y ha desarrollado importantes Temas Científicos.</p>
+                            </div>
+                          </div>
+                        </a>
+                    </li>
+                    <li class="mix">
+                      <a href="#">
+                        <img src="img/Ing. Javier.png" alt="">
+                        <div class="overly">
+                          <div class="position-center">
+                            <h2>MSC. Ing. Javier Peña González</h2>
+                            <p>Ingeniero Hidráulico y Master en ingeniería para el "Manejo de los recursos hidráulicos y el medioambiente". Ha trabajado por más de 22 años como proyectista y director de proyectos para presas y obras hidrotécnicas en general. Posee conocimientos y experiencia en la gestión y explotación de los recursos hídricos por cuencas, en el diseño hidráulico y en la supervisión durante diferentes tipos de obras</p>
+                          </div>
+                        </div>
+                      </a>
+                  </li>
+                  <li class="mix">
+                    <a href="#">
+                      <img src="img/Ing. Yanel.jpg" alt="">
+                      <div class="overly">
+                        <div class="position-center">
+                          <h2>Ing. Yanel Llanes Pozo</h2>
+                          <p>Ingeniero Hidráulico por más de 14 años, con experiencia en consultoría, diseño y construcción de obras de ingeniería, relacionadas con sistemas de alta presión (abastecimiento, saneamiento e hidroenergía), rectificación de causes y dragado - construcción de canales.</p>
+                        </div>
+                      </div>
+                    </a>
+                </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </section>
