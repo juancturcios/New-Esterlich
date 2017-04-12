47ad7bef8a2130855604b8fec18c0e76b7c538b7
juancturcios <juancturcios@hotmail.com>
2017-04-11 23:07:53 -0600

    modal

--- ingenieria.php
+++ ingenieria.php
@@ -81,14 +81,39 @@
               <div class="col-md-4 col-sm-12">
                 <div class="block">
                   <div class="thumbnail sin-fondo">
+                    <a  data-toggle="modal" data-target="#myModal">
                     <img src="img/pet.png" alt="...">
                     <div class="caption">
                       <hr>
                       <h3>Centrales hidroeléctricas</h3>
                     </div>
+                    </a>
                   </div>
                 </div>
               </div>
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Modal Header</h4>
+      </div>
+      <div class="modal-body">
+        <p>Some text in the modal.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
               <div class="col-md-4 col-sm-12">
                 <div class="block">
                   <div >
