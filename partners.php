e442fdd25400d65514cdbb534999bc0a8b4b6e0e
ariasover <ariasover@gmail.com>
2017-04-18 00:52:10 -0600

    Cambios

--- partners.php
+++ partners.php
@@ -4,7 +4,7 @@
  <section id="global-header" class="header-partners">
      <div class="container">
          <div class="row">
-             <div class="col-md-12">
+             <div class="col-sm-12">
                  <div class="block">
                      <h1>PARTNERS</h1>
                      <p>Completamente Especializados</p>
@@ -22,70 +22,122 @@
        </div>
        <hr>
 
-       <div class="col-md-4">
+       <div class="col-sm-4">
          <div class="block">
            <div>
              <h2 class="title-mision">EMIMEX MÉXICO</h2>
              <img src="img/img.png" alt ="EMIMEX MÉXICO">
+             <span class="glyphicon glyphicon-envelope" aria-hidden="true">
+               <a href="mailto:www.gpolytek.com"> www.gpolytek.com</a><br>
+             </span>
+             <br>
+             <span class="glyphicon glyphicon-phone" aria-hidden="true">
+               <a href="tel:5552573990">(+55) 5257 3990</a>
+             </span>
+             <br>
+             <span class="glyphicon glyphicon-phone" aria-hidden="true">
+               <a href="tel:5552573990">(+55) 5257 3995</a>
+             </span>
+             <!-- <p><span class="glyphicon glyphicon-phone"></span> +55 5257 3990</p> -->
 
-              <!-- <p>Envelope icon: <span class="glyphicon glyphicon-envelope"></span></p> -->
            </div>
-           <!-- <p class="text-justify">Ligero, rígido o semi-rígido, transparente o de color natural, el PET es un excelente barrera contra la y los gases ; es además resistente al impacto. Es uno de los plásticos más fácilmente reciclables y se utiliza para fabricar botellas, bolsas y fibras sintéticas para la ropa.</p> -->
          </div>
        </div>
 
-       <div class="col-md-4">
+       <div class="col-sm-4">
          <div class="block">
            <div>
              <h2 class="title-mision">NOVATEX-NICARAGUA</h2>
              <img src="img/img.png" alt ="EMIMEX MÉXICO">
+             <span class="glyphicon glyphicon-envelope" aria-hidden="true">
+               <a href="mailto:info@novatex-group.com"> info@novatex-group.com</a>
+             </span>
+             <br>
+             <span class="glyphicon glyphicon-phone" aria-hidden="true">
+               <a href="tel:5552573990">(+505) 2249-8289</a>
+             </span>
            </div>
          </div>
        </div>
-
-       <div class="col-md-4">
+       <div class="col-sm-4">
          <div class="block">
            <div>
              <h2 class="title-mision">NOVATEX-PERÚ</h2>
              <img src="img/img.png" alt ="EMIMEX MÉXICO">
+             <span class="glyphicon glyphicon-envelope" aria-hidden="true">
+               <a href="mailto:info@novatex-group.com"> info@novatex-group.com</a>
+             </span>
+             <br>
+             <span class="glyphicon glyphicon-phone" aria-hidden="true">
+               <a href="tel:5552573990">(+511) 708 6793</a>
+             </span>
            </div>
          </div>
        </div>
+      </div>
 
-       <div class="col-md-4">
-         <div class="block">
-           <div>
-             <h2 class="title-mision">MESOPLAST-SEREX</h2>
-             <img src="img/img.png" alt ="EMIMEX MÉXICO">
-           </div>
-         </div>
-       </div>
 
-       <div class="col-md-4">
-         <div class="block">
-           <div>
-             <h2 class="title-mision">SODINCO HONDURAS</h2>
-             <img src="img/img.png" alt ="EMIMEX MÉXICO">
-           </div>
 
-           <div>
-             <h2 class="title-mision">PLADESA</h2>
-           </div>
+      <div class="row">
+        <div class="col-sm-4">
+          <div class="block">
+            <div>
+              <h2 class="title-mision">MESOPLAST-SEREX</h2>
+              <img src="img/img.png" alt ="EMIMEX MÉXICO">
+            </div>
+          </div>
+        </div>
 
-           <div>
-             <h2 class="title-mision">GRUPO ARQUETIPO</h2>
-           </div>
-         </div>
-       </div>
+        <div class="col-sm-4">
+          <div class="block">
+            <div>
+              <h2 class="title-mision">SODINCO HONDURAS</h2>
+              <img src="img/img.png" alt ="EMIMEX MÉXICO">
+              <span class="glyphicon glyphicon-envelope" aria-hidden="true">
+                <a href="mailto:www.sodincohn.com"> www.sodincohn.com</a>
+              </span>
+              <br>
+              <span class="glyphicon glyphicon-phone" aria-hidden="true">
+                <p href="tel:5552573990">(+504) 9558-0727</p>
+              </span>
 
-       <div class="col-md-4">
-         <div class="block">
-           <div>
-             <h2 class="title-mision">GRUPO DILMA ESPAÑA</h2>
-             <img src="img/img.png" alt ="EMIMEX MÉXICO">
-           </div>
-         </div>
-       </div>
+            </div>
+
+            <div>
+              <h2 class="title-mision">PLADESA</h2>
+              <span class="glyphicon glyphicon-envelope" aria-hidden="true">
+                <a href="mailto:www.pladesa.com"> www.pladesa.com</a>
+              </span>
+              <br>
+              <span class="glyphicon glyphicon-phone" aria-hidden="true">
+                <a href="tel:5552573990">(+502) 6662-1500</a>
+              </span>
+            </div>
+
+            <div>
+              <h2 class="title-mision">GRUPO ARQUETIPO</h2>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-4">
+          <div class="block">
+            <div>
+              <h2 class="title-mision">GRUPO DILMA ESPAÑA</h2>
+              <img src="img/img.png" alt ="EMIMEX MÉXICO">
+              <span class="glyphicon glyphicon-envelope" aria-hidden="true">
+                <a href="mailto:www.grupodilma.com"> www.grupodilma.com</a>
+              </span>
+              <br>
+              <span class="glyphicon glyphicon-phone" aria-hidden="true">
+                <a href="tel:5552573990">(+504) 9558-0727</a>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
 
 
      </div>
